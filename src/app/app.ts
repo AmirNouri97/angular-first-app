71c6ea5df7dd7135782ff4875e66daefba09fd16
Amirhossein Noori <nouri97dev@gmail.com>
2025-09-15 09:57:27 +0330

    feat:add basic features,user and task compnent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from "./tasks/tasks";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, User, Tasks,CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  users = DUMMY_USERS
+  selectedUserId='u1'
+  user!:any;//todo
+  // user!:{name!:string,avatar!:string,id!:string};//todo
   protected readonly title = signal('first-angular-app');
+  @Output() targetUser = new EventEmitter()
+
+get selectedUser(){
+return this.users.find((user)=>user.id === this.selectedUserId)
+}
+
+  onSelectUser(id:string){
+    console.log(id);
+    this.selectedUserId = id
+    // this.user = DUMMY_USERS.find(user=>user.id === id)
+    
+  }
+
+
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,45 @@
+import { Component , computed, EventEmitter, Input,input ,output,Output,signal} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users'
+
+const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css'
+})
+export class User {
+//  selectedUser = signal(DUMMY_USERS[randomIndex])
+// imagePath = computed(()=>'assets/users/' + this.selectedUser().avatar)
+
+//input decorator
+
+// @Input({required:true}) id! : string;
+// @Input({required:true}) avatar! : string;
+// @Input({required:true}) name!: string;
+@Input({required:true}) user! :{
+  id:string , avatar:string,name:string
+}
+@Output() select = new EventEmitter()
+
+  get imagePath (){
+  return 'assets/users/' + this.user.avatar
+ }  
+
+ onSelectedUser(){
+  this.select.emit(this.user.id)
+ }
+
+//signal way
+// avatar = input.required<string>()
+// name = input.required<string>()
+// select = output<string>()
+// imagePath = computed(()=>'assets/users/' + this.avatar())
+
+
+//  onSelectedUser(){
+//   // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length)
+//   // this.selectedUser.set(DUMMY_USERS[randomIndex]) 
+//  }
+}
